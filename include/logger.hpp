fd4a50609eae3465249b3b55f2090f2b92ebb9aa
Wei Zhang <natezhang@outlook.com>
2016-07-15 01:46:53 +0900

    Adds logger

---
+++ include/logger.hpp
@@ -0,0 +1,23 @@
+#ifndef LOGGER_HPP_INCLUDED
+#define LOGGER_HPP_INCLUDED
+
+#include <iostream>
+#include <string>
+
+namespace napoleon {
+
+class Logger
+{
+public:
+    static inline void logInfo(std::string info) {
+        std::cout << "[INFO]: " << info << std::endl;
+    }
+
+    static inline void logError(std::string error) {
+        std::cerr << "[ERROR]: " << error << std:endl;
+    }
+};
+
+}
+
+#endif // LOGGER_HPP_INCLUDED
